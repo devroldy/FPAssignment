4dd6a350e15cc3251d86c0d2fc11b6bd040a64b0
Harold Lomotey <haroldlomotey@gmail.com>
2017-01-27 02:36:39 -0500

    formatting and commenting

--- fp.rkt
+++ fp.rkt
@@ -1,37 +1,38 @@
-(DEFINE (reverse-general L)
+(DEFINE (reverse-general L)    
         (COND ((null? L) '())
               ((list? L)
-               (append (reverse-general (cdr L))
-               (list (reverse-general (car L)))))
-             (else L)))
-(reverse-general '(1 (2 3) (4 (a (b (c d))))))
+               (append (reverse-general (cdr L))   ;putting switched list element together
+               (list (reverse-general (car L)))))   ;Recursion also included to keep moving to next element
+             (else L)
+         )
+)
+
 
 
 (DEFINE (sum-up-numbers-simple L)
   (cond
 ((null? L) 0)
 ((not (list? L)) 0)
-((not (number? (car L))) (sum-up-numbers-simple (cdr L)))
+((not (number? (car L))) (sum-up-numbers-simple (cdr L)))  ;not a number returns #t and moves to the next element
 (else
- (+ (sum-up-numbers-simple (car L)) (sum-up-numbers-simple (cdr L)))
-        (+ (car L) (sum-up-numbers-simple (cdr L)))
+ (+ (sum-up-numbers-simple (car L)) (sum-up-numbers-simple (cdr L))) ;sums the current atom and recursively gets next atom
+        (+ (car L) (sum-up-numbers-simple (cdr L))) ;sums the recursely got first atom together with the current
 ) )
 )
 
-(sum-up-numbers-simple '(a b 100 (200)))
 
 
 
-(define (suming L)
+
+(define (sum-up-numbers-general L)
   (cond
 ((null? L) 0)
 
-((number? (car L)) (+ (car L) (suming (cdr L))))
+((number? (car L)) (+ (car L) (sum-up-numbers-general (cdr L)))) ;if its a number does summation recursion of atoms
 ((list? (car L))
-(+ (suming (car L)) (suming (cdr L))))
+(+ (sum-up-numbers-general (car L)) (sum-up-numbers-general (cdr L))))   ;gets nested list summation
 (else
-       (+ 0 (suming (cdr L)))
+       (+ 0 (sum-up-numbers-general (cdr L))) ;if its not a number or list sum it up to 0
 ) )
 )
 
-(suming '(a 100 (200) (200)))
